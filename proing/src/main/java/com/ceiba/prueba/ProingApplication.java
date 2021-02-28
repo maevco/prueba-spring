e10bc29572de0fc779f09327a9df3db05f53f4b3
Manuel Efren Villacriz cordoba <ManuelV@emssanar.local>
2021-02-28 07:16:34 -0600

    Version Uno

---
+++ proing/src/main/java/com/ceiba/prueba/ProingApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProingApplication.class, args);
+	}
+
+}
---
+++ proing/src/test/java/com/ceiba/prueba/ProingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
